cdef6e8b3d8bc9874f9fede479ff90dca571aef1
Babalola Paul <pbabalola27@gmail.com>
2026-03-11 21:49:17 +0100

    refactor: enhance header and nav styles, add IDs for sections, and improve hero content layout

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerInput = document.querySelector('#hamburger1-input');
-    const navLinks = document.querySelector('.nav-links');
-    const navItems = document.querySelectorAll('.nav-link');
 
-    if (!hamburgerInput || !navLinks) {
-        console.error("Elements not found!");
-        return;
-    }
+const hamburgerInput = document.querySelector('#hamburger1-input');
+const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-link');
 
-    hamburgerInput.addEventListener('change', () => {
-        navLinks.classList.toggle('active');
-    });
+if (!hamburgerInput || !navLinks) {
+    console.error("Elements not found!");
+}
 
-    navItems.forEach(item => {
-        item.addEventListener('click', () => {
-            navLinks.classList.remove('active');
-            hamburgerInput.checked = false;
-        });
+hamburgerInput.addEventListener('change', () => {
+    navLinks.classList.toggle('active');
+});
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        hamburgerInput.checked = false;
     });
 });
 
-hamburgerInput.addEventListener("click", () => console.log("Document clicked"));
+
+
+const header = document.querySelector("header");
+const hero = document.querySelector("#hero");
+
+window.addEventListener("scroll", () => {
+
+    const heroHeight = hero.offsetHeight;
+    const scrollPosition = window.scrollY;
+
+    let opacity = scrollPosition / heroHeight;
+
+    if (opacity > 1) {
+        opacity = 1;
+    }
+
+    // header.style.background = `rgba(255,255,255,${opacity})`;\
+    header.style.background = `rgba(40,54,82,${opacity})`
+});
